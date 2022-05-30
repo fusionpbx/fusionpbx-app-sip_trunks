2f10ee1c3a9f8ae06c0c7213fb6fe5ce456d70ff
FusionPBX <markjcrane@gmail.com>
2022-05-30 08:59:28 -0600

    Create app_languages.php

---
+++ app_languages.php
@@ -0,0 +1,13 @@
+<?php
+
+$text['title-sip_trunks']['en-us'] = "SIP Trunks";
+$text['title-sip_trunks']['es-cl'] = "";
+$text['title-sip_trunks']['pt-pt'] = "";
+$text['title-sip_trunks']['fr-fr'] = "";
+$text['title-sip_trunks']['pt-br'] = "";
+$text['title-sip_trunks']['pl'] = "";
+$text['title-sip_trunks']['sv-se'] = "";
+$text['title-sip_trunks']['uk'] = "";
+$text['title-sip_trunks']['de-at'] = "";
+
+?>
